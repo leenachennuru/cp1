74ef5b892e0cec3af26cd9dd32c24be5fca35845
leenthegenieass <leenthegenieass@gmail.com>
2014-03-07 22:37:32 +0530

    With comments and Base Case
    Final Commit
    
    Copyright @ Leena Suresh

--- CountSubstringMatch.py
+++ CountSubstringMatch.py
@@ -1,18 +1,21 @@
 from globalfunctions import *
 from string import *
 def countSubStringMatch(target,key):
-    count = 0
-    while find(target,key) > -1:
-        count += 1
-        target = target[find(target,key)+len(key):]
-    print count
+    count = 0                                           #Initialize Counter
+    if find(target,key) == -1:
+        print "There are no instances of this substring in the target string"   #Base Case
+    else:
+        while find(target,key) > -1:
+            count += 1                                      #Increment the counter
+            target = target[find(target,key)+len(key):]     #Strip the traget down
+        print count
 
 
 def countSubStringMatchRecursive (target, key):
-    if find(target,key) == -1:
+    if find(target,key) == -1:          #Base Case
         return 0
     else:
-        return 1 + countSubStringMatchRecursive (target[find(target,key)+len(key):], key)
+        return 1 + countSubStringMatchRecursive (target[find(target,key)+len(key):], key) #Apply the function recursively on the smaller string
                 
 
     
