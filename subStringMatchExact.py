b84e760c3cf4ea74e22829ce2979bb85f93ace43
leenthegenieass <leenthegenieass@gmail.com>
2014-03-07 22:38:09 +0530

    With Comments and Base Case added
    Final Commit
    
    Copyright @ Leena Suresh

--- subStringMatchExact.py
+++ subStringMatchExact.py
@@ -1,15 +1,15 @@
 from string import *
 from globalfunctions import *
 def subStringMatchExact(target,key):
-    length = len(target)
+    length = len(target)        
     listst = []
-    if find(target,key) == -1:
+    if find(target,key) == -1:      #Test for Base Case
         print "There are no instances of the substring in the target string"
         return None
     else:
         while find(target,key) > -1:
-            listst = listst + [length - len(target) + find(target,key)]
-            target = target[find(target,key)+len(key):]
+            listst = listst + [length - len(target) + find(target,key)]     #Add the length of the stripped string 
+            target = target[find(target,key)+len(key):]                     #Strip the taarget down
         return listst
         
 target = readVal(str, "Please enter the target string ", "Oops! That's not a string! Try again")
