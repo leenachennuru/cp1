5cb74e25244d685ed9cd8e1ec0963ff66d8e2c21
leenthegenieass <leenthegenieass@gmail.com>
2014-03-07 23:59:08 +0530

    Final Commit
    Copyright@ Leena Suresh

--- subStringMatchExact.py
+++ subStringMatchExact.py
@@ -5,12 +5,12 @@ def subStringMatchExact(target,key):
     listst = []
     if find(target,key) == -1:      #Test for Base Case
         print "There are no instances of the substring in the target string"
-        return None
+    
     else:
         while find(target,key) > -1:
             listst = listst + [length - len(target) + find(target,key)]     #Add the length of the stripped string 
             target = target[find(target,key)+len(key):]                     #Strip the taarget down
-        return listst
+    return listst
         
 target = readVal(str, "Please enter the target string ", "Oops! That's not a string! Try again")
 key = readVal(str, "Please enter the Key string ", "Oops! That's not a string! Try again")
